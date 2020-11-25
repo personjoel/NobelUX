37283371a95da9d7226572de8f5c8a192bf05ba0
James Frih <jamesfrih@me.com>
2020-11-25 13:39:03 +0100

    added index router that changes depending on date

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,24 @@ var router = express.Router();
 
 /* GET start page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Homepage :)' });
+  var date = new Date('November 25, 2020 12:00:00 GMT');
+  
+  var dateNow = new Date();
+  dateNow.setHours(dateNow.getHours() + 1);
+  
+  console.log(date);
+  console.log(dateNow);
+
+  if (dateNow >= date) {
+    res.render('result', { title: 'Results' });
+  } else {
+    res.render('index', { title: 'Canditates' });
+  }
 });
 
 /* GET candidate display page. */
 router.get('/candidate', function(req, res, next) {
   res.render('candidate', { title: 'Candidates page :)' });
-});
-
-/* GET result/winner display page. */
-router.get('/result', function(req, res, next) {
-  res.render('result', { title: 'Results page :)' });
 });
 
 /* GET submitted page. */

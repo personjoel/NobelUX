c79d481f325650967cefa73636f75c891cd231dd
GustavMaki <gustav.siljamaki@gmail.com>
2020-11-26 11:15:58 +0100

    added stuff

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/candidate', candidateRouter);
+app.use('/candidate/:id', candidateRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

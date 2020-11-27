40c5c3caa89b65138ece08c4b47fb85f35109e0f
James Frih <jamesfrih@me.com>
2020-11-27 11:16:25 +0100

    obli. commit

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -3,6 +3,9 @@ var dummyData = [
     {id: 2, name: "James Frih", shortinfo:"Hej mitt namn e james frih och jag gillar mig själv.", liv: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."},
     {id: 3, name: "Albin Korell", shortinfo:"Hej mitt namn e james frih och jag gillar James Frih.", liv: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}]
 
+
+
+    
 for(var i=0; i < dummyData.length ; i++){
     const li = document.createElement("p");
     li.classList.add("homepageCandidate");

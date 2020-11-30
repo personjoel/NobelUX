13d5bfb8e833d4de2601c1f62ac75b87366901f5
James Frih <jamesfrih@me.com>
2020-11-30 13:05:45 +0100

    added code that determines a winner

--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -1,9 +1,36 @@
-fetch(`https://nobeldata.herokuapp.com/nobel/4`)
+/**
+var resultId;
+
+fetch(`https://nobeldata.herokuapp.com/vote`)
 .then(response => response.json())
 .then(data => {
+    console.log(data);
+    var res = Math.max.apply(Math, data.map(function(o) { return o.votes; }))
+    var obj = data.find(function(o){ return o.votes == res; })
+    
+    resultId = obj.id;
+
+    console.log(resultId);
+
+    myFunction(resultId);
+});
+
+function myFunction(resultId) {
+    fetch(`https://nobeldata.herokuapp.com/nobel/${resultId}`)
+    .then(response => response.json())
+    .then(data => {
     document.getElementById("name").innerHTML= data.firstname + ' ' + data.lastname;
     document.getElementById("content").innerHTML= data.shortinfo;
     var img = document.getElementById('imgCandidate');
     img.src = 'images/test.png';
-    //document.getElementById('mainContent').appendChild(img);
+    });
+}
+*/
+fetch(`https://nobeldata.herokuapp.com/nobel/4`)
+.then(response => response.json())
+.then(data => {
+document.getElementById("name").innerHTML= data.firstname + ' ' + data.lastname;
+document.getElementById("content").innerHTML= data.shortinfo;
+var img = document.getElementById('imgCandidate');
+img.src = 'images/test.png';
 });

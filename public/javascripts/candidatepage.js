616a232a5c52c0331c760bef5ab2f2ae90328f70
GustavMaki <gustav.siljamaki@gmail.com>
2020-12-02 10:10:47 +0100

    right pic in candidate pages

--- public/javascripts/candidatepage.js
+++ public/javascripts/candidatepage.js
@@ -5,6 +5,7 @@ console.log(id);
 fetch(`https://nobeldata.herokuapp.com/nobel/${id}`)
 .then(response => response.json())
 .then(data => {
+    document.getElementById("testImg").src = data.imagecode;
     document.getElementById("candidatePageName").innerHTML= data.firstname + ' ' + data.lastname;
     document.getElementById("candidatePageShortDesc").innerHTML= data.shortinfo;
     document.getElementById("candidatePageLifeDesc").innerHTML= data.liv;

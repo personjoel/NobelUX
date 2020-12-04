244f31df66f03e5d72c60d461e13efdb37427640
James Frih <jamesfrih@me.com>
2020-12-04 13:00:46 +0100

    temporary fix

--- routes/candidate.js
+++ routes/candidate.js
@@ -3,6 +3,26 @@ var router = express.Router();
 var dateCheck = require('../node_functions/date.js');
 
 router.get('/:id', function(req, res, next) {
+  var date = new Date('December 4, 2021 12:55:00 GMT');
+  
+  var dateNow = new Date();
+  dateNow.setHours(dateNow.getHours() + 1);
+
+  var dates = [date, dateNow];
+
+  console.log(dates);
+
+  if (dateNow[1] >= date[0]) {
+    res.statusCode = 302;
+    res.setHeader("Location", "https://nobel-app.herokuapp.com/");
+    res.end();
+  } else {
+    res.render('candidate', { title: 'Canditates' });
+  }
+});
+
+/**
+router.get('/:id', function(req, res, next) {
   dateCheck.method();
   
   if (dateCheck.dates[1] >= dateCheck.dates[0]) {
@@ -13,5 +33,5 @@ router.get('/:id', function(req, res, next) {
     res.render('candidate', { title: 'Ind. candidate page', id: req.params.id });
   }
 });
-
+*/
 module.exports = router;
